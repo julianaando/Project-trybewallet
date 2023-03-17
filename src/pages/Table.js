3a202bfa908efa795ee6c15d5b3697f019a7df51
Juliana Ando <juandocezare@gmail.com>
2023-03-17 20:59:59 -0300

    Req 7 done

---
+++ src/pages/Table.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+/*
+A tabela deve ser alimentada pelo estado da aplicação, que estará disponível na chave expenses
+que vem do reducer wallet:
+O campo de Moeda deverá conter o nome da moeda.
+Portanto, ao invés de 'USD' ou 'EUR', deve conter "Dólar Americano/Real Brasileiro" e "Euro/Real Brasileiro", respectivamente;
+O elemento que exibe a Moeda de conversão deverá ser sempre 'Real';
+Atenção também às casas decimais dos campos.
+Como são valores contábeis, eles devem apresentar duas casas após o ponto.
+Arredonde sua resposta somente na hora de renderizar o resultado e, para os cálculos,
+utilize sempre os valores vindos da API (utilize o campo ask que vem da API).
+Utilize sempre o formato 0.00 (número - ponto - duas casas decimais).
+9 <td> e 1 <tr> para cada despesa.
+ */
+
+class Table extends Component {
+  render() {
+    const { expenses } = this.props;
+    return (
+      <table data-testid="div-table">
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          {expenses.map((expense) => (
+            <tr key={ expense.id }>
+              <td>{expense.description}</td>
+              <td>{expense.tag}</td>
+              <td>{expense.method}</td>
+              <td>{(+expense.value).toFixed(2)}</td>
+              <td>{expense.exchangeRates[expense.currency].name}</td>
+              <td>{(+expense.exchangeRates[expense.currency].ask).toFixed(2)}</td>
+              <td>
+                {(expense.value * expense.exchangeRates[expense.currency]
+                  .ask).toFixed(2)}
+              </td>
+              <td>Real</td>
+              <td>
+                <button type="button">Editar</button>
+                <button type="button">Excluir</button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+Table.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    value: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    currency: PropTypes.string.isRequired,
+    method: PropTypes.string.isRequired,
+    tag: PropTypes.string.isRequired,
+    exchangeRates: PropTypes.arrayOf(PropTypes.shape({
+      code: PropTypes.string.isRequired,
+      codein: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      high: PropTypes.string.isRequired,
+      low: PropTypes.string.isRequired,
+      varBid: PropTypes.string.isRequired,
+      pctChange: PropTypes.string.isRequired,
+      bid: PropTypes.string.isRequired,
+      ask: PropTypes.string.isRequired,
+      timestamp: PropTypes.string.isRequired,
+      create_date: PropTypes.string.isRequired,
+    })).isRequired,
+  })).isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+export default connect(mapStateToProps)(Table);
